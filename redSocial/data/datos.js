ba477d37449b95ad58a679e8e58ad388c1e6bb53
MarcosLagreste <mlagrestezucchini@udesa.edu.ar>
2021-09-16 09:44:08 -0300

    datos.js completa

--- redSocial/data/datos.js
+++ redSocial/data/datos.js
@@ -4,18 +4,43 @@ const usuario = {
                 nombreDeUsuario: "Brunogrecco",
                 seguidores: 150,
                 seguidos: 150,
-                numeroDePublicaciones: 5,
+                numeroDePublicaciones: 2,
                 id: 1,
                 imagenPerfil:""
             },
             {
-                nombreDeUsuario: "tomasmorales",
+                nombreDeUsuario: "Tomasmorales",
                 seguidores: 150,
                 seguidos: 150,
-                numeroDePublicaciones: 5,
+                numeroDePublicaciones: 2,
                 id: 2,
                 imagenPerfil:""
-            }
+            },
+            {
+                nombreDeUsuario: "Marcoslagreste",
+                seguidores: 150,
+                seguidos: 150,
+                numeroDePublicaciones: 2,
+                id: 3,
+                imagenPerfil:""
+            },            
+            {
+                nombreDeUsuario: "Juanperez",
+                seguidores: 150,
+                seguidos: 150,
+                numeroDePublicaciones: 2,
+                id: 4,
+                imagenPerfil:""
+            },            
+            {
+                nombreDeUsuario: "Josegomez",
+                seguidores: 150,
+                seguidos: 150,
+                numeroDePublicaciones: 2,
+                id: 5,
+                imagenPerfil:""
+            },
+
     ]
 
 }
