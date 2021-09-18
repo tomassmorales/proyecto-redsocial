123fe9943ba7ade973383ede2d6e139f2bc248fc
brunogrecco <bgrecco@udesa.edu.ar>
2021-09-18 13:15:38 -0300

    usuarios

--- redSocial/data/datos.js
+++ redSocial/data/datos.js
@@ -15,7 +15,31 @@ const usuario = {
                 numeroDePublicaciones: 5,
                 id: 2,
                 imagenPerfil:""
-            }
+            },
+            {
+                nombreDeUsuario: "marcoslagreste",
+                seguidores: 150,
+                seguidos: 150,
+                numeroDePublicaciones: 5,
+                id: 2,
+                imagenPerfil:""
+            },
+            {
+                nombreDeUsuario: "lautarorodriguez",
+                seguidores: 150,
+                seguidos: 150,
+                numeroDePublicaciones: 5,
+                id: 2,
+                imagenPerfil:""
+            },
+            {
+                nombreDeUsuario: "Javiermartinez",
+                seguidores: 150,
+                seguidos: 150,
+                numeroDePublicaciones: 5,
+                id: 2,
+                imagenPerfil:""
+            },
     ]
 
 }
