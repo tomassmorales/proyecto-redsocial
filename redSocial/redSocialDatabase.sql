cf744b97a350d71354d1814896e2963bf7bed77f
MarcosLagreste <mlagrestezucchini@udesa.edu.ar>
2021-09-23 11:01:55 -0300

    DataBaseC

--- redSocial/redSocialDatabase.sql
+++ redSocial/redSocialDatabase.sql
@@ -10,3 +10,13 @@ fecha_creacion DATETIME NOT NULL,
 
 FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
+
+CREATE TABLE comentarios (
+id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+usuario_id INT UNSIGNED,
+posteo_id VARCHAR(200) NOT NULL, 
+texto VARCHAR(40),
+fecha_creacion DATETIME NOT NULL,
+
+FOREIGN KEY (posteo_id) REFERENCES comentarios (comentarioID)
+);
