8404daf04459dddd17052f7da47a6ca52afd4423
Tomas <morales@MacBook-Air-de-Tomas.local>
2021-09-25 10:29:51 -0300

    detalle usuario y fotos perfil

--- redSocial/data/datos.js
+++ redSocial/data/datos.js
@@ -6,7 +6,7 @@ const usuario = {
                 seguidos: 150,
                 numeroDePublicaciones: 5,
                 id: 1,
-                imagenPerfil:""
+                imagenPerfil:"https://c.wallhere.com/photos/12/ce/mel_gibson_celebrity_face_smile_eyes-717975.jpg!d"
             },
             {
                 nombreDeUsuario: "tomasmorales",
@@ -14,7 +14,7 @@ const usuario = {
                 seguidos: 150,
                 numeroDePublicaciones: 5,
                 id: 2,
-                imagenPerfil:""
+                imagenPerfil:"https://lh3.googleusercontent.com/proxy/n08FU339sllVzHI9Sdzoxaqke68upvVMxNDwMXYVdZMKDnSpfk0-iaSUxnBma8VusjKMxIQ_1CHnwrx7h6-THkr9hDd8VVJVwTlTMxvvI_W_Rrr56IXAy4UOkKZzTWiNo1z_syH3"
             },
             {
                 nombreDeUsuario: "marcoslagreste",
@@ -22,23 +22,23 @@ const usuario = {
                 seguidos: 150,
                 numeroDePublicaciones: 5,
                 id: 3,
-                imagenPerfil:""
+                imagenPerfil:"https://static2.abc.es/media/200912/14/davidmeca--300x180.jpg"
             },
             {
-                nombreDeUsuario: "lautarorodriguez",
+                nombreDeUsuario: "juanagonzales",
                 seguidores: 150,
                 seguidos: 150,
                 numeroDePublicaciones: 5,
                 id: 4,
-                imagenPerfil:""
+                imagenPerfil:"https://hips.hearstapps.com/ellees.h-cdn.co/assets/15/37/1024x1332/1024x1332-r-ti-rostros-dilatados-personas-energicas-12718630-1-esl-es-rostros-dilatados-personas-energicas-jpg.jpg?resize=480:*"
             },
             {
-                nombreDeUsuario: "Javiermartinez",
+                nombreDeUsuario: "anamartinez",
                 seguidores: 150,
                 seguidos: 150,
                 numeroDePublicaciones: 5,
                 id: 5,
-                imagenPerfil:""
+                imagenPerfil:"https://i.pinimg.com/736x/c8/e9/72/c8e972106158f5dd52b0b20c6292f089.jpg"
             },
     ]
 
