9dba900086bdf4b8cc95be2194cc9c89e28bad60
Tomas <morales@MacBook-Air-de-Tomas.local>
2021-09-25 09:45:46 -0300

    cambios archivo sql

--- redSocial/redSocialDatabase.sql
+++ redSocial/redSocialDatabase.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE redSocial;
 USE redSocial;
 
-CREATE TABLE usuarios {
+CREATE TABLE usuarios (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 seguidores INT UNSIGNED DEFAULT(0),
 seguidos INT UNSIGNED DEFAULT(0),
 publicaciones INT UNSIGNED DEFAULT(0),
 fotoPerfil VARCHAR(2000),
-
-}
+);
 
 CREATE TABLE posts (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -23,8 +22,10 @@ FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 CREATE TABLE comentarios (
 id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 posteo_id VARCHAR(200) NOT NULL, 
+usuario_id INT UNSIGNED,
 texto VARCHAR(40),
 fecha_creacion DATETIME NOT NULL,
 
+FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
 FOREIGN KEY (posteo_id) REFERENCES posts(id)
 );
