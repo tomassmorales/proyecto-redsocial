80bfe92a2efb0b51eb964a58d17a32ba7d819d65
Tomas <morales@MacBook-Air-de-Tomas.local>
2021-10-21 09:51:33 -0300

    post model

--- redSocial/database/models/Post.js
+++ redSocial/database/models/Post.js
@@ -24,13 +24,13 @@ module.exports = function(sequelize, dataTypes){
 	    }
 	}
     
-	let configuracionDeLaTabla = {
+	let conf= {
 	    tableName: 'posts', 
 	    timestamps: false, //Si la tabla no tiene los campos created_at y updated_at
 	    underscored: true, //Si los nombres de las columnas en la db tienen guiones bajos en lugar de camelCase.        
 	}
     
-       const Movie = sequelize.define(alias, columnas, configuracionDeLaTabla);
+       const Post = sequelize.define(alias, columnas, conf);
     
-       return Movie;
+       return Post;
     }
