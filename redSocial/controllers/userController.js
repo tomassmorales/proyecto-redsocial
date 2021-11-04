aef34ea4ee97fca8299a082424052e696c0af9c1
Tomas <morales@MacBook-Air-de-Tomas.local>
2021-11-04 16:44:54 -0300

    login

--- redSocial/controllers/userController.js
+++ redSocial/controllers/userController.js
@@ -52,6 +52,42 @@ let userController = {
 	},
 	login: function (req, res) {
 		res.render('login')
+	},
+	logeo: function(req,res){
+
+		let errors = {};
+
+		if (req.body.email == "") {
+		    errors.message = "El campo del email no puede estar vacio";
+		    res.locals.error = errors;
+		    res.render("login")
+		} else {
+		    db.Usuario.findOne({
+			    where: {
+				email: req.body.email
+			    }
+			})
+			.then(function(user) {
+			    if (user != undefined) {
+				let passwordCorrecta = bcrypt.compareSync(req.body.password, user.password)
+				if (passwordCorrecta == true) {
+				    req.session.user = user.email;
+				    if (req.body.recordame) {
+					res.cookie("usuarioId", user.id, {
+					    maxAge: 1000 * 60 * 30
+					}) //30 minutos
+				    }
+				    return res.redirect("/")
+				} else {
+				    return res.redirect("/user/registracion")
+				}
+			    }
+			})
+			.catch(err => {
+			    console.log(err);
+			    res.send(err)
+			})
+		}
 	}
 
 }
