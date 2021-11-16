c88193327d8b69e9559dcd7f9ff269e390f505fa
MarcosLagreste <mlagrestezucchini@udesa.edu.ar>
2021-11-16 17:23:56 -0300

    config.js

---
+++ redSocial/database/config/config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "redSocial",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "operatorsAliases": false,
+    "port": 3306
+ },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
